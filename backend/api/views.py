2cb5bf0c4f604da4946db5f91e77c9fcd45dc4dd
MattIgo <mcigo330@gmail.com>
2025-02-25 20:28:48 -0500

    Connected frontend to backend api users

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,12 +8,15 @@ import random
 
 
 class UserCreate(APIView):
+    print("Hello, Client!")
+
     serializer_class = CreateUserSerializer
 
     def post(self, request, format=None):
         serializer = self.serializer_class(data=request.data)
         
-        #Make sure to enter valid email
+        # Make sure to enter valid email
+        # Does it check for duplicates of unique values?
         if serializer.is_valid():
             first_name = serializer.validated_data['first_name']
             last_name = serializer.validated_data['last_name']
@@ -21,10 +24,12 @@ class UserCreate(APIView):
             email = serializer.validated_data['email']
             user_id = random.randint(111111111, 999999999)
 
+            #TODO: check if generated used ID is already in use 
+
             user = User(user_id=user_id, first_name=first_name, last_name=last_name, email=email, password=password)
             user.save()
 
             return Response(UserSerializer(user).data, status=status.HTTP_200_OK)
 
-        return Response("Enter Valid Email", status=status.HTTP_404_NOT_FOUND)
+        return Response("Enter Valid Input", status=status.HTTP_400_BAD_REQUEST)
 
--- frontend/src/components/Signuppage.tsx
+++ frontend/src/components/Signuppage.tsx
@@ -1,29 +1,68 @@
-// TODO: functions to add user to database
+// TODO: functions to add user to database/ hooks etc
 
 const Signuppage = (): JSX.Element => {
 
-    return (
-      <div className="signupContainer">
-        <div className="signupBox">
-            <h1>Sign up to Create an Account</h1>
-              <form id="signupform">
-                <label style={{marginLeft: 100}}>First Name: </label>
-                <input className="signuplabel" type="text" id="firstname" name="firstname"></input><br/>
-                <label style={{marginLeft: 100}}>Last Name: </label>
-                <input className="signuplabel" type="text" id="lastname" name="lastname"></input><br/>
-                <label style={{marginLeft: 100}}>Email: </label>
-                <input className="signuplabel" type="text" id="email" name="email"></input><br/>
-                <label style={{marginLeft: 100}}>Username: </label>
-                <input className="signuplabel" type="text" id="signupuser" name="signupuser"></input><br/>
-                <label style={{marginLeft: 100}}>Password: </label>
-                <input className="signuplabel" type="text" id="signuppass" name="signuppass"></input><br/>
-                <label style={{marginLeft: 100}}>Re-enter Password: </label>
-                <input className="signuplabel" type="text" id="resignuppass" name="resignuppass"></input><br/>
-                <input style={{marginLeft: 200}} type="submit" id="signup" name="signup" value="Sign up"></input>
-              </form>
-        </div>
-      </div>
-    );
-  }
+  const handleSignup = (e: React.ChangeEvent<any>) => {
+    // Prevent page from reloading and disrupting fetch
+    e.preventDefault();
+
+    // Check for null values
+    if ((document.getElementById('firstname') as HTMLInputElement).value == null ||
+      (document.getElementById('lastname') as HTMLInputElement).value == null ||
+      (document.getElementById('email') as HTMLInputElement).value == null ||
+      (document.getElementById('signuppass') as HTMLInputElement).value == null) {
+        return;
+    }
+
+    // Check for empty Strings
+    if ((document.getElementById('firstname') as HTMLInputElement).value == '' ||
+      (document.getElementById('lastname') as HTMLInputElement).value == '' ||
+      (document.getElementById('email') as HTMLInputElement).value == '' ||
+      (document.getElementById('signuppass') as HTMLInputElement).value == '') {
+        alert("Enter All Credentials");
+        return;
+    }
+
+    // TODO: Cyclic error
+    let firstName = (document.getElementById('firstname') as HTMLInputElement).value;
+    let lastName = (document.getElementById('lastname') as HTMLInputElement).value;
+    let email = (document.getElementById('email') as HTMLInputElement).value;
+    let password = (document.getElementById('signuppass') as HTMLInputElement).value;
+
+    fetch('http://localhost:8000/users', {
+      method: 'POST',
+      headers: { "Content-Type" : "application/json" },
+      body: JSON.stringify({"first_name" : firstName,
+        "last_name" : lastName,
+        "password" : password,
+        "email" : email
+      }),
+    }).then(()=> {
+      alert('User Registered!');
+    })
+  } 
   
-  export default Signuppage;
+  return (
+    <div className="signupContainer">
+      <div className="signupBox">
+        <h1>Sign up to Create an Account</h1>
+          <form id="signupform">
+            <label style={{marginLeft: 100}}>First Name: </label>
+            <input className="signuplabel" type="text" id="firstname" name="firstname"></input><br/>
+            <label style={{marginLeft: 100}}>Last Name: </label>
+            <input className="signuplabel" type="text" id="lastname" name="lastname"></input><br/>
+            <label style={{marginLeft: 100}}>Email: </label>
+            <input className="signuplabel" type="text" id="email" name="email"></input><br/>
+            <label style={{marginLeft: 100}}>Password: </label>
+            <input className="signuplabel" type="text" id="signuppass" name="signuppass"></input><br/>
+            <label style={{marginLeft: 100}}>Re-enter Password: </label>
+            <input className="signuplabel" type="text" id="resignuppass" name="resignuppass"></input><br/>
+            <input style={{marginLeft: 200}} type="submit" id="signup" name="signup" value="Sign-up"
+              onClick={(e)=>handleSignup(e)}></input>
+          </form>
+      </div>
+    </div>
+  );
+}
+  
+export default Signuppage;
