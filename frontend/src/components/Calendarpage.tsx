4a6033493cebc3d2ec305ed9957cc2dbf841d8bc
MattIgo <mcigo330@gmail.com>
2026-01-21 23:26:16 -0500

    Working on Search Notes function

--- frontend/src/components/Calendarpage.tsx
+++ frontend/src/components/Calendarpage.tsx
@@ -6,6 +6,8 @@ const Calendarpage = (): JSX.Element => {
   const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
   let [monthIndex, setMonthIndex] = useState(currentDate.getMonth());
   let [year, setYear] = useState(currentDate.getFullYear());
+  let [searchParameters, setSearchParameters] = useState <string[]>(['', '', '']);
+  let [orderDropdown, setOrderDropdown] = useState("Order By...");
 
   const handleLeftButton = (e: React.MouseEvent) => {
     e.preventDefault();
@@ -31,6 +33,38 @@ const Calendarpage = (): JSX.Element => {
     setMonthIndex(newMonthIndex);
   }
 
+  const getNotes = () => {
+    console.log(searchParameters + "," + orderDropdown);
+
+    //TODO: check for valid dates
+  }
+
+  const updateSearchArray = (event: React.ChangeEvent<HTMLInputElement> | React.MouseEvent<HTMLDivElement, MouseEvent>, parameter: string) => {
+    if (event.type === "change") {
+      const inputElement = event.target as HTMLInputElement;
+
+      // Which Search Parameter is being updated
+      if (parameter === "noteWords") {
+        const updatedSearchParameters = [...searchParameters];
+        updatedSearchParameters[0] = inputElement.value;
+        setSearchParameters(updatedSearchParameters);
+      } else if (parameter === "startDate") {
+        const updatedSearchParameters = [...searchParameters];
+        updatedSearchParameters[1] = inputElement.value;
+        setSearchParameters(updatedSearchParameters);
+      } else if (parameter === "endDate") {
+        const updatedSearchParameters = [...searchParameters];
+        updatedSearchParameters[2] = inputElement.value;
+        setSearchParameters(updatedSearchParameters);
+      } else {
+        console.log("error in parameter checking on updateSearchArray");
+      }
+    } else {
+      const divElement = event.target as HTMLDivElement;
+      setOrderDropdown(divElement.textContent);
+    }
+  }
+
   return (
     <div className="calendarPageContainer">
       <div id="month">{months[monthIndex]} {year}</div>
@@ -40,20 +74,23 @@ const Calendarpage = (): JSX.Element => {
 
       <div id="searchNotesContainer">
         <div id="searchBarNotesContainer">
+          <h1 id="searchNotesTitle">Search Notes for Keywords or Phrases</h1>
           <div id="searchBarContainer">
-            <input id="searchNotesInput" placeholder="Search Notes"></input>
-            <button className="searchButton">Search</button>
+            <input id="searchNotesInput" placeholder="Type Word or Phrase" onChange={(e) => updateSearchArray(e, "noteWords")}></input>
+            <button className="searchButton" onClick={() => getNotes()}>Search</button>
           </div>
           <div id="searchParametersContainer">
-            <h1>Dates:</h1>
-            <input placeholder="Enter Dates"></input>
-            <h1>Order By:</h1>
-            <div id="orderDropdown">Order By...
+            <h2>Start Date:</h2>
+            <input placeholder="mm-dd-yyyy" id="enterStartDateInput" onChange={(e) => updateSearchArray(e, "startDate")}></input>
+            <h2>End Date:</h2>
+            <input placeholder="mm-dd-yyyy" id="enterEndDateInput" onChange={(e) => updateSearchArray(e, "endDate")}></input>
+            <h2>Order By:</h2>
+            <div id="orderDropdown">
+              <h2 id="selectedSortOption">{orderDropdown}</h2>
               <div id="orderDropdownOptions">      
-                <div className="orderDropdownIndividualOption">Oldest</div>  
-                <div className="orderDropdownIndividualOption">Latest</div>
-                <div className="orderDropdownIndividualOption">Recency</div> 
-                <div className="orderDropdownIndividualOption">1</div>
+                <div className="orderDropdownIndividualOption" onClick={(e) => updateSearchArray(e, "")}>Oldest</div>  
+                <div className="orderDropdownIndividualOption" onClick={(e) => updateSearchArray(e, "")}>Latest</div>
+                <div id="lastOrderOption" className="orderDropdownIndividualOption" onClick={(e) => updateSearchArray(e, "")}>Recency</div> 
               </div>
             </div>
           </div>
