5647320f12d50f41a728c22906ec28d389aa002a
MattIgo <mcigo330@gmail.com>
2026-07-14 23:22:19 -0400

    1

---
+++ database/testSQL.sql
@@ -0,0 +1,35 @@
+INSERT INTO public.user (user_id, first_name, last_name, email, password)
+VALUES (385697154, 'Matt', 'Igo', 'mcigo332@gmail.com', 'Otto1234');
+
+insert into public.user_note (note_id, message, user_id)
+values (111111111, 'hihihihi', '385697154');
+
+set search_path
+
+select * 
+from public.user;
+
+select *
+from public.authtoken_token;
+
+
+select * 
+from public.user_note;
+
+delete from "user"
+where "email" = 'mcigo332@gmail.com';
+
+delete from "user_note";
+
+drop table "user";
+
+drop table "user_note";
+
+drop schema Workflow;
+
+select * 
+from public.authtoken_token;
+
+delete from authtoken_token;
+
+/* used tables: user, auth_token*/
